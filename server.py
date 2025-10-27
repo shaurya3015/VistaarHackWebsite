c6ed6a1809fcfcf3b57197564413e2b19fcca8a2
lokesh <purohitlucky135@gmail.com>
2025-10-27 21:26:58 +0530

    hugging face

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from langchain_huggingface import HuggingFaceEmbeddings, ChatHuggingFace, Huggin
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 
+
 from quiz_backend import register_quiz_routes
 
 # ===============================
@@ -43,7 +44,7 @@ chat_history = []
 # ===============================
 print("Loading embeddings model...")
 hf_embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_name="sentence-transformers/all-mpnet-base-v2",
     model_kwargs={"device": "cpu"},
     encode_kwargs={"normalize_embeddings": True},
 )
@@ -221,5 +222,6 @@ if __name__ == "__main__":
     print("Starting Flask AI server in debug mode on http://127.0.0.1:5000")
     # Port 5000 is fine for local. Render/Gunicorn will ignore this.
     app.run(port=5000, debug=True)
+
 
 
